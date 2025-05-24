7a1366890eb8e9b8690738fbd16c3ad09cee544b
kazimayzid <kazimayzid208@gmail.com>
2025-05-25 01:50:50 +0600

    i am working on input section

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,6 +4,7 @@ import { BsPatchCheck } from "react-icons/bs";
 import { IoAlarmOutline } from "react-icons/io5";
 import { AiOutlineFire } from "react-icons/ai";
 import { IoIosArrowDown } from "react-icons/io";
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 export default function Banner() {
   const bannerDes = [
@@ -18,7 +19,7 @@ export default function Banner() {
     {
       icon: <AiOutlineFire />,
       pra: "Transparent, Affordable Pricing",
-    }
+    },
   ];
   return (
     <>
@@ -34,18 +35,53 @@ export default function Banner() {
                   {bannerDes.map((items) => (
                     <div className="flex items-center gap-x-2">
                       <span className="text-white">{items.icon}</span>
-                      <p className="font-secondary font-normal text-[16px] leading-6 tracking-[.01%] text-white">{items.pra}</p>
+                      <p className="font-secondary font-normal text-[16px] leading-6 tracking-[.01%] text-white">
+                        {items.pra}
+                      </p>
                     </div>
                   ))}
                 </div>
               </div>
               <div className="bg-white p-[10px]">
-                <input className="w-[180px] border-background rounded-[4px] border-[.5px] py-[10px] pl-[12px] placeholder:font-secondary placeholder:font-normal placeholder:text-[14px] placeholder:leading-[20px] placeholder:tracking-[.02%] placeholder:text-secondary placeholder:py-[10px] placeholder:pl-[12px] focus:outline-none" type="text" placeholder="Name"/>
-                <input className="w-[180px] border-background rounded-[4px] border-[.5px] py-[10px] pl-[12px] placeholder:font-secondary placeholder:font-normal placeholder:text-[14px] placeholder:leading-[20px] placeholder:tracking-[.02%] placeholder:text-secondary placeholder:py-[10px] placeholder:pl-[12px] focus:outline-none" type="number" placeholder="Phone"/>
-                <div className="relative">
-                  <input className="w-[180px] border-background rounded-[4px] border-[.5px] py-[10px] pl-[12px] placeholder:font-secondary placeholder:font-normal placeholder:text-[14px] placeholder:leading-[20px] placeholder:tracking-[.02%] placeholder:text-secondary placeholder:py-[10px] placeholder:pl-[12px] focus:outline-none" type="text" placeholder="Service"/><IoIosArrowDown  className="absolute"/>
+                <input
+                  className="w-[180px] border-background rounded-[4px] border-[.5px] py-[10px] pl-[12px] placeholder:font-secondary placeholder:font-normal placeholder:text-[14px] placeholder:leading-[20px] placeholder:tracking-[.02%] placeholder:text-secondary placeholder:py-[10px] placeholder:pl-[12px] focus:outline-none"
+                  type="text"
+                  placeholder="Name"
+                />
+                <input
+                  className="w-[180px] border-background rounded-[4px] border-[.5px] py-[10px] pl-[12px] placeholder:font-secondary placeholder:font-normal placeholder:text-[14px] placeholder:leading-[20px] placeholder:tracking-[.02%] placeholder:text-secondary placeholder:py-[10px] placeholder:pl-[12px] focus:outline-none"
+                  type="number"
+                  placeholder="Phone"
+                />
+                <div className="relative inline-block w-[180px]">
+                  <select
+                    className="appearance-none w-full border-background rounded-[4px] border-[.5px] py-[10px] pl-[12px] font-secondary font-normal text-[14px] leading-[24px] tracking-[.02%] text-secondary outline-none"
+                    name=""
+                    id="2"
+                  >
+                    <option
+                      className="text-secondary bg-background font-secondary text-[14px]"
+                      value="2"
+                    >
+                      Service
+                    </option>
+                    <option
+                      className="text-secondary bg-background font-secondary text-[14px]"
+                      value="2"
+                    >
+                      Product
+                    </option>
+                    <option
+                      className="text-secondary bg-background font-secondary text-[14px]"
+                      value="2"
+                    >
+                      Others
+                    </option>
+                  </select>
+                  <div className="absolute top-1/2 -translate-1/2 right-[40%]">
+                    <RiArrowDropDownLine className="text-secondary text-[30px]" />
+                  </div>
                 </div>
-
               </div>
               <div></div>
             </div>
